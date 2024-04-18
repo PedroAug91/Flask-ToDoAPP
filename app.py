8b281145bbddb63c3551c74c32be34ba13c9c0c9
Pedro Augusto <95659638+PedroAug91@users.noreply.github.com>
2024-04-18 19:57:08 -0300

    Update app.py
    função de cadastro

--- app.py
+++ app.py
@@ -39,3 +39,46 @@ def home():
         return render_template("home.html", title="Tarefas", active_tasks=tarefas_ativas, inactive_tasks=tarefas_inativas)
 
     return render_template("home.html", title="Tarefas", active_tasks=None, inactive_tasks=None)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    error = None
+
+    if (request.method == "POST"):
+        nome = request.form["username"]
+        email = request.form["email"]
+        senha = request.form["password"]
+
+        conexao = connect_to_database()
+        cursor = conexao.cursor(dictionary=True)
+
+        sql ="SELECT * FROM usuario WHERE email = %s"
+        valores = (email, )
+        cursor.execute(sql, valores) 
+        usuario = cursor.fetchone()
+        
+        if (usuario is not None):
+            error = "Este email já foi cadastrado, tente outro."
+        else:
+            sql = "INSERT INTO usuario (nome, email, senha) VALUES (%s, %s, %s)"
+            valores = (nome, email, senha)
+
+
+            cursor.execute(sql, valores)
+            conexao.commit()
+            
+            sql = "SELECT id_usuario FROM usuario WHERE email = %s"
+            valores = (email, )
+            cursor.execute(sql, valores)
+
+            id_usuario = cursor.fetchone()
+
+            cursor.close()
+            conexao.close()
+
+            session["nome"] = nome
+            session["id_usuario"] = id_usuario["id_usuario"]
+            return redirect(url_for("home"))
+
+    return render_template("signup.html", title="Cadastrar", error=error)
+
